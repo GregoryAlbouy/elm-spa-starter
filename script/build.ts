74f4d91288ecdbef544a89280589e620e589f14e
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-07-03 17:25:05 +0200

    Refactor script: build

--- script/build.ts
+++ script/build.ts
@@ -7,50 +7,65 @@ import { sassPlugin } from "esbuild-sass-plugin";
 import postcss from "postcss";
 import postcssPresetEnv from "postcss-preset-env";
 
-const watch = process.argv.includes("--watch");
-const isProduction = process.env.NODE_ENV === "production";
-
-const SRC_DIR = "./src";
-const OUT_DIR = "./dist";
-const STATIC_DIR = "./static";
-
-function fromSource(localPath: string) {
-  return path.resolve(SRC_DIR, localPath);
+enum Directory {
+  SOURCE = "./src",
+  OUTPUT = "./dist",
+  STATIC = "./static",
 }
 
-(function main() {
-  esbuild
-    .build({
-      entryPoints: {
-        app: fromSource("app.ts"),
-        "assets/style/main": fromSource("scss/main.scss"),
-      },
-      bundle: true,
-      outdir: OUT_DIR,
-      minify: isProduction,
-      watch,
-      sourcemap: !isProduction && "inline",
-      plugins: [
-        copyStaticFilesPlugin({
-          src: STATIC_DIR,
-          dest: OUT_DIR,
-        }),
-        elmPlugin({
-          debug: !isProduction,
-          optimize: isProduction,
-          clearOnWatch: watch,
-        }),
-        sassPlugin({
-          type: "css",
-          sourceMap: !isProduction,
-          async transform(source: string, _: string) {
-            const { css } = await postcss([
-              postcssPresetEnv({ stage: 2 }),
-            ]).process(source);
-            return css;
-          },
-        }),
-      ],
-    })
-    .catch(() => process.exit(1));
+function pathFromSource(localPath: string) {
+  return path.resolve(Directory.SOURCE, localPath);
+}
+
+function getBuildOptions({
+  production,
+  watch,
+}: {
+  production: boolean;
+  watch: boolean;
+}): esbuild.BuildOptions {
+  return {
+    entryPoints: {
+      app: pathFromSource("app.ts"),
+      "assets/style/main": pathFromSource("scss/main.scss"),
+    },
+    bundle: true,
+    outdir: Directory.OUTPUT,
+    minify: production,
+    watch,
+    sourcemap: !production && "inline",
+    plugins: [
+      copyStaticFilesPlugin({
+        src: Directory.STATIC,
+        dest: Directory.OUTPUT,
+      }),
+      elmPlugin({
+        debug: !production,
+        optimize: production,
+        clearOnWatch: watch,
+      }),
+      sassPlugin({
+        type: "css",
+        sourceMap: !production,
+        async transform(source: string, _: string) {
+          const { css } = await postcss([
+            postcssPresetEnv({ stage: 2 }),
+          ]).process(source);
+          return css;
+        },
+      }),
+    ],
+  };
+}
+
+(async function main() {
+  const production = process.env.NODE_ENV === "production";
+  const watch = process.argv.includes("--watch");
+  const buildOptions = getBuildOptions({ production, watch });
+
+  try {
+    await esbuild.build(buildOptions);
+  } catch (_: unknown) {
+    process.exit(1);
+  }
 })();
